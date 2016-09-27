eec00ccf61569aa0ac363d8a689dd2484919b99c
Nikita Dubko <mefody93@gmail.com>
2016-09-27 09:44:25 +0300

    Fix mistakes

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,62 @@
 $(function() {
-  // Smooth scroll to :target links
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return true;
-      }
-    }
-  });
+    // Smooth scroll to :target links
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return true;
+            }
+        }
+    });
 });
 
 
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('google-map'), {
-    center: {lat: 53.8892491, lng: 27.5756478},
-    zoom: 17,
-    styles: [
-    {
-      "featureType": "all",
-      "elementType": "all",
-      "stylers": [
-      {
-        "saturation": -100
-      },
-      {
-        "gamma": 0.5
-      }
-      ]
-    }
-    ]
-  });
-  var marker = new google.maps.Marker({
-    position: {lat: 53.8892491, lng: 27.5756478},
-    map: map,
-    title: 'Фотошкола Павла Бабарыкина',
-    icon: 'images/png/placeholder-small.png'
-  });
-  var contentString = '<div id="content">'+
-  '<div id="siteNotice">'+
-  '</div>'+
-  '<h1 id="firstHeading" class="firstHeading">Фотошкола Павла Бабарыкина</h1>'+
-  '<div id="bodyContent">'+
-  '<p>Some test text</p>'+
-  '</div>'+
-  '</div>';
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 400
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-}
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('google-map'), {
+        center: {
+            lat: 53.8892491,
+            lng: 27.5756478
+        },
+        zoom: 17,
+        scrollwheel: false,
+        styles: [{
+            "featureType": "all",
+            "elementType": "all",
+            "stylers": [{
+                "saturation": -100
+            }, {
+                "gamma": 0.5
+            }]
+        }]
+    });
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 53.8892491,
+            lng: 27.5756478
+        },
+        map: map,
+        title: 'Фотошкола Павла Бабарыкина',
+        icon: 'images/png/placeholder-small.png'
+    });
+    var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Фотошкола Павла Бабарыкина</h1>' +
+        '<div id="bodyContent">' +
+        '<p>Some test text</p>' +
+        '</div>' +
+        '</div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 400
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
