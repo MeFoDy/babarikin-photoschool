5a79bb4efc069c390d6dedf76c30ed10202bdfee
Nikita Dubko <mefody93@gmail.com>
2016-12-19 02:59:44 +0300

    Optimize total page size

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ gulp.task('copy:fonts', function() {
 
 gulp.task('copy:js', function() {
     return gulp.src('app/scripts/**.js')
+        .pipe(uglify())
         .pipe(gulp.dest('dist/scripts/'));
 });
 
