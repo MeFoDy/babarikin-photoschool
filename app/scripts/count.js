c865ac2ca545ae4a14e51c5718cb5b25c550296e
Nikita Dubko <mefody93@gmail.com>
2017-02-22 23:12:21 +0300

    Update discount block

--- app/scripts/count.js
+++ app/scripts/count.js
@@ -3,14 +3,14 @@ window.statSettings = {
 	hasPlacesCount: 3, // количество свободных мест
 
 	/*     Блок "Нам стоит доверять"     */
-	trustYears: 13, // опыт съемки у преподавателя
-	trustCompanies: 79, // компаний отправили сотрудников на обучение
+	trustYears: 15, // опыт съемки у преподавателя
+	trustCompanies: 85, // компаний отправили сотрудников на обучение
 	trustPercent: 100, // процентов результат
-	trustCount: 2813, // прошли обучение
+	trustCount: 2868, // прошли обучение
 
-	oldPrice: 46, // старая цена
-	newPrice: 29, // новая цена
-	finalDate: '30 января' // дата конца скидки
+	oldPrice: 289, // старая цена
+	newPrice: 159, // новая цена
+	finalDate: '8 марта' // дата конца скидки
 
 };
 window.statSettings.discount = ((window.statSettings.oldPrice - window.statSettings.newPrice) * 100 / window.statSettings.oldPrice).toFixed(0);
